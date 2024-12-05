9172ae1b09fa1935357231c20753ced9f68b903b
Prakritig01 <prakritigupta1112002@gmail.com>
2024-12-06 00:41:55 +0530

    UI improved

--- src/Components/JobComponent/JobComponent.js
+++ src/Components/JobComponent/JobComponent.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { JobContext } from "./../Context/JobConetxt";
 import "./JobComponent.css";
 import companyImg from "./../../assests/Employers Logo (1).png";
+import bookmark from "../../assests/BookmarkSimple.svg";
 
 const JobComponent = () => {
   const { jobList } = useContext(JobContext); // Get the jobList from context
@@ -16,10 +17,16 @@ const JobComponent = () => {
             <p className="salary-para">{job.salary}</p>
           </div>
           <div className="company">
-            <img src={companyImg} alt="" />
-            <div className="company-text">
-              <p>{job.company}</p>
-              <p>{job.location}</p>
+            <div className="left">
+              <img src={companyImg} alt="" className="company-logo" />
+              <div className="company-text">
+                <p>{job.company}</p>
+                <p>{job.location}</p>
+              </div>
+            </div>
+
+            <div className="right">
+              <img src={bookmark} alt="" className="bookmark" />
             </div>
           </div>
         </div>
