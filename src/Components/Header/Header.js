eebd525c829bffa1ee163cba1364a4bf3f62c6b5
Prakritig01 <prakritigupta1112002@gmail.com>
2024-12-05 17:30:14 +0530

    header added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css";
+import bellRing from "./../../assests/BellRinging (1).png";
+import ellipse from "./../../assests/Ellipse 18.png";
+import briefCase from "./../../assests/briefcase 1.png";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="heading">
+        <img src={briefCase} alt="" srcset="" className="brief-case" />
+        <p>Exporin Solutions</p>
+      </div>
+      <div className="navigation">
+
+        <div className="saved-jobs">
+          <button className="btn"> </button>
+          <p>My Saved Jobs only</p>
+        </div>
+
+        <div className="images-div">
+          <img src={bellRing} alt="" srcset="" className="bellRing" />
+          <img src={ellipse} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
